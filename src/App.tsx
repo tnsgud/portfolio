1fd0c6dd7ab8f894f503d44f2f87f24134859096
tnsgud <qkrtnsgud0229@gmail.com>
2022-08-12 14:58:35 +0900

    간단하게 베너 만들고 있는 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'css/App.css';
+import Typewriter from 'typewriter-effect';
+import { useState } from 'react';
 
 function App() {
+  const [isDone, setIsDone] = useState(false);
+  const information: string[] = [
+    '이름 : 박순형',
+    '생일 : 2004.02.29',
+    '학교 : 인천전자마이스터 고등학교 (재학중)',
+    'Skills : Java, React, Vue, Typescript, Flutter',
+  ];
+  const navigationCaption: string[] = ['소개', '활동', '자격증', '수상경력'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>박순형</h1>
+        <ul>
+          {navigationCaption.map((caption) => (
+            <li key={caption}>
+              <a className='nav' href={`#${caption}`}>
+                {caption}
+              </a>
+            </li>
+          ))}
+        </ul>
       </header>
+      <main>
+        <section className='informationSection'>
+          <div className='informationDiv'>
+            {isDone && <img id='photo' src='/image/profile.jpg' alt='' />}
+            {isDone &&
+              information.map((info) => (
+                <Typewriter
+                  key={info}
+                  onInit={(typewriter) => {
+                    typewriter.typeString(info).start();
+                  }}
+                  options={{
+                    cursor: '',
+                    wrapperClassName: 'information',
+                  }}
+                />
+              ))}
+          </div>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('프론트 엔드 개발자를<br/>')
+                .pauseFor(100)
+                .typeString('꿈꾸는<br/>')
+                .pauseFor(100)
+                .typeString('박순형입니다.')
+                .start()
+                .callFunction(() => {
+                  setIsDone(true);
+                });
+            }}
+            options={{
+              wrapperClassName: 'title',
+            }}
+          />
+        </section>
+      </main>
     </div>
   );
 }
