40d4e29a33b3c82911d09a9c781ae4f3201910fb
tnsgud <qkrtnsgud0229@gmail.com>
2022-08-12 15:22:17 +0900

    파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
 import 'css/App.css';
 import Typewriter from 'typewriter-effect';
-import { useState } from 'react';
+import Main from 'components/main';
 
 function App() {
-  const [isDone, setIsDone] = useState(false);
   const information: string[] = [
     '이름 : 박순형',
     '생일 : 2004.02.29',
     '학교 : 인천전자마이스터 고등학교 (재학중)',
     'Skills : Java, React, Vue, Typescript, Flutter',
   ];
-  const navigationCaption: string[] = ['소개', '활동', '자격증', '수상경력'];
+  const navigationCaption: string[] = [
+    'main',
+    '소개',
+    '활동',
+    '자격증',
+    '수상경력',
+  ];
+  const components: JSX.Element[] = [<Main />];
 
   return (
     <div>
@@ -28,41 +33,29 @@ function App() {
         </ul>
       </header>
       <main>
-        <section className='informationSection'>
-          <div className='informationDiv'>
-            {isDone && <img id='photo' src='/image/profile.jpg' alt='' />}
-            {isDone &&
-              information.map((info) => (
-                <Typewriter
-                  key={info}
-                  onInit={(typewriter) => {
-                    typewriter.typeString(info).start();
-                  }}
-                  options={{
-                    cursor: '',
-                    wrapperClassName: 'information',
-                  }}
-                />
-              ))}
-          </div>
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString('프론트 엔드 개발자를<br/>')
-                .pauseFor(100)
-                .typeString('꿈꾸는<br/>')
-                .pauseFor(100)
-                .typeString('박순형입니다.')
-                .start()
-                .callFunction(() => {
-                  setIsDone(true);
-                });
-            }}
-            options={{
-              wrapperClassName: 'title',
-            }}
-          />
-        </section>
+        {navigationCaption.map((caption, i) => {
+          return (
+            <section key={i} id={caption}>
+              {caption}
+              {components[i]}
+            </section>
+          );
+        })}
+        <div className='informationDiv'>
+          <img id='photo' src='/image/profile.jpg' alt='' />
+          {information.map((info) => (
+            <Typewriter
+              key={info}
+              onInit={(typewriter) => {
+                typewriter.typeString(info).start();
+              }}
+              options={{
+                cursor: '',
+                wrapperClassName: 'information',
+              }}
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ src/components/main.tsx
@@ -0,0 +1,23 @@
+import Typewriter from 'typewriter-effect';
+import 'css/main.css';
+
+const Main = () => {
+  return (
+    <Typewriter
+      onInit={(typewriter) => {
+        typewriter
+          .typeString('프론트 엔드 개발자를<br/>')
+          .pauseFor(100)
+          .typeString('꿈꾸는<br/>')
+          .pauseFor(100)
+          .typeString('박순형입니다.')
+          .start();
+      }}
+      options={{
+        wrapperClassName: 'title',
+      }}
+    />
+  );
+};
+
+export default Main;
