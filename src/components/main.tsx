2e2aa963eb9065c88afc0d4f602de56408094eb9
tnsgud <qkrtnsgud0229@gmail.com>
2022-09-05 10:58:06 +0900

    Main.tsx button 추가

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,7 +1,13 @@
 import 'css/Main.css';
+import { useState, MouseEvent } from 'react';
 import { TypingMultiline } from 'react-kr-typing-anim';
 
 const Main = () => {
+  const [isDone, setIsDone] = useState(false);
+
+  const onAboutClick = (e: MouseEvent<HTMLButtonElement>) => {
+    alert('on click');
+  };
   return (
     <div className='title'>
       <TypingMultiline
@@ -9,12 +15,17 @@ const Main = () => {
         preDelay={1000}
         postDelay={1000}
         fixedWidth
-        onDone={() => console.log('done')}
+        onDone={() => setIsDone(true)}
         cursor
         strs={`프론트엔드를
       꿈꾸는
       박순형입니다.`}
       />
+      {isDone && (
+        <button className='aboutButton' onClick={onAboutClick}>
+          About me
+        </button>
+      )}
     </div>
   );
 };
