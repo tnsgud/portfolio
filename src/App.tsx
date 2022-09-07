92c6c4cb509474a59bc6b2b3f2ca142a8f14a908
tnsgud <qkrtnsgud0229@gmail.com>
2022-09-08 08:18:07 +0900

    pageable 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,121 @@
-import 'css/App.css';
-import Main from 'components/Main';
-import About from 'components/About';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { MouseEvent, useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 function App() {
-  const navigationCaption: string[] = ['main', 'about'];
-  const components: JSX.Element[] = [<Main />, <About />];
-  const [navigators, setNavigators] = useState<boolean[]>(
-    Array(components.length).fill(false)
-  );
+  const containerRef = useRef<HTMLDivElement>(null);
+  const linearProgressBarRef = useRef<HTMLDivElement>(null);
+  const pageableRef = useRef<any>(null);
+
+  const loadPageable = async () => {
+    const Pageable = (await import('pageable')).default;
+    const pageable = new Pageable(containerRef.current, {
+      childSelector: '[data-anchor]',
+      anchors: [],
+      pips: true,
+      animation: 300,
+      delay: 0,
+      throttle: 50,
+      orientation: 'vertical' as 'vertical' | 'horizontal',
+      swipeThreshold: 50,
+      freeScroll: false,
+      navPrevEl: false,
+      navNextEl: false,
+      infinite: true,
+      events: {
+        wheel: true,
+        mouse: true,
+        touch: true,
+        keydown: true,
+      },
+      easing: (currentTime, startPos, endPos, interval) => {
+        return (
+          -endPos * (currentTime /= interval) * (currentTime - 2) + startPos
+        );
+      },
+      onScroll: (data) => {
+        updateLinearProgressBar(data);
+      },
+      // onInit: () => {},
+      // onUpdate: (data) => {}
+      // onBeforeStart: () => {},
+      // onStart: () => {},
+      // onFinish: () => {}
+    });
+
+    pageableRef.current = pageable;
+  };
+
+  const updateLinearProgressBar = (data) => {
+    const pos = 1 - (data.max - data.scrolled) / data.max;
+    const linearProgressBar = linearProgressBarRef.current;
+    if (!linearProgressBar) return;
+    linearProgressBar.style.transform = `scale(${pos}, 1)`;
+  };
 
   useEffect(() => {
-    window.addEventListener('wheel', mouseWheelEvent);
-    return () => {
-      window.removeEventListener('wheel', mouseWheelEvent);
-    };
-  });
-
-  useEffect(() => {
-    let arr = Array(2).fill(false);
-    arr[0] = true;
-    setNavigators(arr);
+    loadPageable();
   }, []);
 
-  const changeNavigation = (index: number) => {
-    if (index < 0 || index > navigators.length - 1) {
-      return;
-    }
-
-    let arr: boolean[] = Array(navigators.length).fill(false);
-
-    arr[index] = true;
-
-    const element = document.getElementById(`${index}`) as HTMLAnchorElement;
-    element.scrollIntoView();
-
-    setNavigators(arr);
-  };
-
-  const onNavigatorClick = (e: MouseEvent<HTMLAnchorElement>) => {
-    changeNavigation(Number(e.currentTarget.id));
-  };
-
-  const mouseWheelEvent = (e: WheelEvent) => {
-    const currentIndex = navigators.indexOf(true);
-
-    if (e.deltaY < 0) {
-      changeNavigation(currentIndex - 1);
-    } else if (e.deltaY > 0) {
-      changeNavigation(currentIndex + 1);
-    }
-  };
-
   return (
-    <div className='scroll-container'>
-      <header>
-        {navigators.map((isSelected, index) => (
-          <a
-            key={index}
-            id={`${index}`}
-            onClick={onNavigatorClick}
-            href={`#${navigationCaption[index]}`}
-            className='navigator'
-            style={{ color: `${isSelected ? 'white' : 'gray'}` }}
-          >
-            ●
-          </a>
-        ))}
-      </header>
-      <div>
-        {components.map((component, index) => (
+    <>
+      <main className='indexPage pg-wrapper'>
+        <div id='container' className='pg-container' ref={containerRef}>
           <div
-            key={index}
-            id={`${navigationCaption[index]}`}
-            className='scroll-area'
-          >
-            {component}
-          </div>
-        ))}
-      </div>
-    </div>
+            data-anchor='page-1'
+            id='page-1'
+            className='pg-page'
+            style={{
+              backgroundColor: 'peachpuff',
+            }}
+          ></div>
+          <div
+            data-anchor='page-2'
+            className='pg-page'
+            style={{
+              backgroundColor: 'cornflowerblue',
+            }}
+          ></div>
+          <div
+            data-anchor='page-3'
+            className='pg-page'
+            style={{
+              backgroundColor: 'bisque',
+            }}
+          ></div>
+          <div
+            data-anchor='page-4'
+            className='pg-page'
+            style={{
+              backgroundColor: 'mediumaquamarine',
+            }}
+          ></div>
+        </div>
+
+        <div
+          className='linear-progress'
+          style={{
+            position: 'fixed',
+            top: '0px',
+            left: '10px',
+            width: 'calc(100vw - 20px)',
+            height: '2px',
+          }}
+        >
+          <div
+            className='bar'
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+              backgroundColor: '#84e8ff',
+              transformOrigin: '0 0 0',
+            }}
+            ref={linearProgressBarRef}
+          ></div>
+        </div>
+      </main>
+    </>
   );
 }
 
